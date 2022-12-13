14a534f54a9515652f169569b1f68b2b93f42d15
muhammedemreagda <119903051+muhammedemreagda@users.noreply.github.com>
2022-12-13 11:15:13 +0300

    Update writeNameinaRow.c

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,19 @@
 // Online C compiler to run C program online
 #include <stdio.h>
-int main(){
- int i,j,k,n; 
- printf("Enter the number of the row :");
- scanf("%d",&n);
- 
+#include <stdlib.h>
+
+int main() {
+	int i,j,k,l,n,m; 
+	printf("Enter three number(row,column,matris): ");
+ scanf("%d%d%d",&n,&m,&l);
  for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
-    
+		 for(j=0;j<l;j++){
+			  for(k=0;k<m;k++){
+   		printf("Emre ");
+		   }
+		 printf(" ");
+		 }
+ printf("\n");
+	}
+	return 0;
 }
